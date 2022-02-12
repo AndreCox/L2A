215d5ba881ae72077c463f970ca750ea7825402d
Andre <arc.xoc@gmail.com>
2022-02-11 21:58:28 -0800

    Working Kinda

--- PiCarV/server/server.js
+++ PiCarV/server/server.js
@@ -4,24 +4,17 @@ const io = new Server();
 
 io.on("connection", (socket) => {
   console.log("Client Connected");
-  console.log(socket.data);
 
-  // client-side
-  let count = 0;
-  setInterval(() => {
-    socket.volatile.emit("steering", ++count);
-    if (count > 180) {
-      count = 0;
-    }
-  }, 50);
+  const sndPower = (power) => {
+    socket.volatile.emit("power", power);
+  };
 
-  let speed = -100;
-  setInterval(() => {
-    socket.volatile.emit("drive", ++speed);
-    if (speed > 100) {
-      speed = -100;
-    }
-  }, 50);
+  const sndSteer = (steer) => {
+    socket.volatile.emit("steering", steer);
+  };
+
+  socket.on("drive", (data) => sndPower(data)); //It's about drive it's about power
+  socket.on("steer", (data) => sndSteer(data));
 });
 
 io.listen(3000);
